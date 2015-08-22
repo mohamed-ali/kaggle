f587e4d950b10b46fcafd361a0911c721e86f3ed
mohamed-ali <m.ali.jamaoui@gmail.com>
2015-08-22 17:45:07 +0100

    refactoring to OOP

--- Bag of words meets bags of popcorn/scripts/exploring_bag_of_words_OOP.py
+++ Bag of words meets bags of popcorn/scripts/exploring_bag_of_words_OOP.py
@@ -3,104 +3,95 @@ import re
 import pandas as pd
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords 
-
-data_path="C:\\Users\\MedAli\\Desktop\\kaggle_competitions\\Bag of words meets bags of popcorn\\data\\"
-
-train = pd.read_csv(data_path+"labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
-#quoting=3 to ignore the doubled quotes
-
-###print the dimensions of the data: rows and columns
-##mm = train.shape
-##print mm[0]
-###see the column names
-##print train.columns.values
-##
-###see few reviews
-##print train["review"][0]
-##
-###data cleaning and text processing
-##
-###[train["review"][i] = BeautifulSoup(train["review"]).get_text() for i in xrange(mm[0])]
-##
-##print train["review"][0]
-
-def review_to_words(raw_review):
-
-    review_text = BeautifulSoup(raw_review).get_text()
-
-    review_text = re.sub("[0-9\.]+", "NUM", review_text) 
-    letters_only = re.sub("[^a-zA-Z]", " ", review_text)
-
-    
-    words = letters_only.lower().split()
-
-    stops = set(stopwords.words("english"))
-
-    meaningful_words = [w for w in words if w not in stops]
-
-    return " ".join(meaningful_words)
-
-
-
-#clean_review = review_to_words(train["review"][0])
-
-
-
-num_reviews = train["review"].size
-
-clean_train_reviews = []
-
-print "Cleaning and parsin the training set movie reviews...\n"
-for i in xrange(0, num_reviews):
-    if((i+1)%1000 == 0):
-        print "review %d of %d\n" % (i+1, num_reviews)
-    clean_train_reviews.append(review_to_words(train["review"][i]))
-    
-
-
-print "creating the bag of words \n"
+#for bag of words
 from sklearn.feature_extraction.text import CountVectorizer
-
-vectorizer = CountVectorizer(analyzer="word", tokenizer = None, preprocessor= None, stop_words = None, max_features = 1000)
-
-train_data_features = vectorizer.fit_transform(clean_train_reviews)
-
-train_data_features = train_data_features.toarray()
-
-print "dimension of train_data_features:", train_data_features.shape
-
-#vocab = vectorizer.get_feature_names()
-
-print "training the random forest"
-
 from sklearn.ensemble import RandomForestClassifier
 
-forest = RandomForestClassifier(n_estimators = 100)
+class KaggleBagOfPopcorn():
+    def __init__(self):
+        pass
 
-forest = forest.fit(train_data_features, train["sentiment"])
+    #load csv to pandas dataframe 
+    def loadData(self,data_file):
+        return pd.read_csv(data_path+"labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
 
-print "preparing a submission"
 
-test= pd.read_csv(data_path+"testData.tsv", header=0, delimiter="\t", quoting=3)
+    def ReviewToWords(self):
+        review_text = BeautifulSoup(raw_review).get_text()
+        review_text = re.sub("[0-9\.]+", "NUM", review_text) 
+        letters_only = re.sub("[^a-zA-Z]", " ", review_text)
+        words = letters_only.lower().split()
+        stops = set(stopwords.words("english"))
+        meaningful_words = [w for w in words if w not in stops]
+        return " ".join(meaningful_words)
 
-print "test shape", test.shape
+    def ApplyBagOfWords(self,cleaned_data):
+        vectorizer = CountVectorizer(analyzer="word", tokenizer = None, preprocessor= None, stop_words = None, max_features = 1000)
+        train_data_features = vectorizer.fit_transform(cleaned_data)
+        train_data_features = train_data_features.toarray()
+        return train_data_features
 
-num_reviews = len(test["review"])
-clean_test_reviews = []
 
-print "clearning and parsing the test set movie reviews..\n"
-for i in xrange(0, num_reviews):
-    if((i+1)%1000 == 0):
-        print "review %d of %d\n" % (i+1, num_reviews)
-    clean_review = review_to_words(test["review"][i])
-    clean_test_reviews.append(clean_review)
+    def ApplyRandomForestClassifier(self,train, train_data_features, test_data_features):
+        forest = RandomForestClassifier(n_estimators = 100)
+        forest = forest.fit(train_data_features, train["sentiment"])
+        result = forest.predict(test_data_features)
+        return result
 
-test_data_features = vectorizer.transform(clean_test_reviews)
-test_data_features = test_data_features.toarray()
+    def PrepareSubmission(self,test, result):
+        output = pd.DataFrame(data={"id":test["id"], "sentiment": result}) 
+        output.to_csv("Bag_of_words_model.csv", index=False, quoting=3)
 
-result = forest.predict(test_data_features)
 
-output = pd.DataFrame(data={"id":test["id"], "sentiment": result})
- 
-output.to_csv("Bag_of_words_model.csv", index=False, quoting=3)
+    def main():
+        #load train set 
+        data_path="C:\\Users\\MedAli\\Desktop\\kaggle_competitions\\Bag of words meets bags of popcorn\\data\\"
+        train = self.loadData(data_path)
+
+        #load test set        
+        test= self.loadData(data_path+"testData.tsv")
+
+        num_reviews = train["review"].size
+
+        #cleaning train set 
+        clean_train_reviews = []
+        print "Cleaning and parsing the training set movie reviews...\n"
+        for i in xrange(0, num_reviews):
+            if((i+1)%1000 == 0):
+                print "review %d of %d\n" % (i+1, num_reviews)
+            clean_train_reviews.append(self.ReviewToWords(train["review"][i]))
+
+        print "creating the bag of words \n"
+        train_data_features = self.ApplyBagOfWords(clean_train_reviews)
+
+        print "dimension of train_data_features:", train_data_features.shape
+
+        #vocab = vectorizer.get_feature_names()
+
+        #cleaning testset 
+        num_reviews = len(test["review"])
+        clean_test_reviews = []
+
+        print "clearning and parsing the test set movie reviews..\n"
+        for i in xrange(0, num_reviews):
+            if((i+1)%1000 == 0):
+                print "review %d of %d\n" % (i+1, num_reviews)
+            clean_review = self.ReviewToWords(test["review"][i])
+            clean_test_reviews.append(clean_review)
+
+        #applying bag of words on the test data
+        test_data_features = self.ApplyBagOfWords(clean_test_reviews)
+
+        #applying random forest 
+        result = self.ApplyRandomForestClassifier(train, train_data_features, test_data_features)
+
+        #prepare Csv file for submission
+        self.PrepareSubmission(test, result)
+
+        return 
+
+
+if __name__ == '__main__':
+    bagOfpopcorn = KaggleBagOfPopcorn()
+    bagOfpopcorn.main()
 
